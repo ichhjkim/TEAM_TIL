454348458d147abd455a7c7bab9e74a36cdaa0e5
maximus <maximus@maximusui-MacBookPro.local>
2022-05-01 19:00:18 +0900

    TacoCloudSample Basic Setting

---
+++ spring/Jin/taco-cloud3/src/main/java/tacos/TacoCloud3Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloud3Application.class, args);
+	}
+
+}
